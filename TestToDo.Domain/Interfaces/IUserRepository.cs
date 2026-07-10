59f5bd3f645a717f975d10461ef78e0ff1305a17
xcesoo <notxceso@gmail.com>
2026-07-10 14:24:43 +0300

    feat(user-repo): implemented user interface repo

---
+++ TestToDo.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+using TestToDo.Entities;
+
+namespace TestToDo.Interfaces;
+
+public interface IUserRepository
+{
+    Task<IReadOnlyCollection<User>> GetUsersAsync(CancellationToken cancellationToken);
+    Task<User?> GetUserByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<User?> GetUserByEmailAsync(string email, CancellationToken cancellationToken);
+    Task AddUserAsync(User user, CancellationToken cancellationToken);
+    Task DeleteUserAsync(Guid id, CancellationToken cancellationToken);
+    Task SaveChangesAsync(CancellationToken cancellationToken);
+}
