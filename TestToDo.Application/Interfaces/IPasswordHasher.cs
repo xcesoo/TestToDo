177a476dc814495dbd7de828d8ccf52d96a290f5
xcesoo <notxceso@gmail.com>
2026-07-10 21:42:47 +0300

    feat(auth): implemented bcrypt password hasher

--- TestToDo.Application/Interfaces/IPasswordHasher.cs
+++ TestToDo.Application/Interfaces/IPasswordHasher.cs
@@ -2,6 +2,6 @@ namespace TestToDo.Application.Interfaces;
 
 public interface IPasswordHasher
 {
-    Task<string> Hash(string password);
-    Task<bool> Verify(string hashedPassword, string password);
+    string Hash(string password);
+    bool Verify(string hashedPassword, string password);
 }
---
+++ TestToDo.Infrastructure/Auth/BcryptPasswordHasher.cs
@@ -0,0 +1,16 @@
+using TestToDo.Application.Interfaces;
+
+namespace TestToDo.Infrastructure.Auth;
+
+public class BcryptPasswordHasher : IPasswordHasher
+{
+    public string Hash(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool Verify(string password, string hashedPassword)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+    }
+}
